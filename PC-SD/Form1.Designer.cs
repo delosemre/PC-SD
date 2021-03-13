5b652121a093240fe0ad3561c917b8cfb30736ad
Emre Yılmaz <delosemre@outlook.com>
2021-03-13 15:04:18 +0300

    Proje dosyası ekle.

---
+++ PC-SD/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace PC_SD
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.zamanla = new System.Windows.Forms.Button();
+            this.zaman = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // zamanla
+            // 
+            resources.ApplyResources(this.zamanla, "zamanla");
+            this.zamanla.Name = "zamanla";
+            this.zamanla.UseVisualStyleBackColor = true;
+            this.zamanla.Click += new System.EventHandler(this.zamanla_Click);
+            // 
+            // zaman
+            // 
+            resources.ApplyResources(this.zaman, "zaman");
+            this.zaman.Name = "zaman";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zaman);
+            this.Controls.Add(this.zamanla);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button zamanla;
+        private System.Windows.Forms.TextBox zaman;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PC-SD/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+
+namespace PC_SD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void zamanla_Click(object sender, EventArgs e)
+        {
+            int sure = Convert.ToInt32(zaman.Text);
+            int dakika = 60;
+            int kapanmasure = sure * dakika;
+            Process.Start("shutdown", "/s /t " + kapanmasure);
+            MessageBox.Show(sure + " dakika içerisinde bilgisayar kapanacak!");
+        }
+    }
+}
